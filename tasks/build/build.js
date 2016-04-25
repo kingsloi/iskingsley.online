b2e624d212dc4f86ffe7c52881f25cf3661d7df6
Kingsley Raspe <kingsley.raspe@googlemail.com>
2016-04-25 17:05:58 +0100

    added ignored files

---
+++ tasks/build/build.js
@@ -0,0 +1,121 @@
+'use strict';
+
+var pathUtil = require('path');
+var Q = require('q');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var batch = require('gulp-batch');
+var plumber = require('gulp-plumber');
+var jetpack = require('fs-jetpack');
+
+var bundle = require('./bundle');
+var generateSpecImportsFile = require('./generate_spec_imports');
+var utils = require('../utils');
+
+var projectDir = jetpack;
+var srcDir = projectDir.cwd('./app');
+var destDir = projectDir.cwd('./build');
+
+var paths = {
+    copyFromAppDir: [
+        './node_modules/**',
+        './helpers/**',
+        './**/*.html',
+        './**/*.+(jpg|png|svg)',
+        './fonts/**',
+        './js/jquery.min.js'
+    ],
+};
+
+// -------------------------------------
+// Tasks
+// -------------------------------------
+
+gulp.task('clean', function () {
+    return destDir.dirAsync('.', { empty: true });
+});
+
+
+var copyTask = function () {
+    return projectDir.copyAsync('app', destDir.path(), {
+            overwrite: true,
+            matching: paths.copyFromAppDir
+        });
+};
+gulp.task('copy', ['clean'], copyTask);
+gulp.task('copy-watch', copyTask);
+
+
+var bundleApplication = function () {
+    return Q.all([
+            bundle(srcDir.path('background.js'), destDir.path('background.js')),
+            bundle(srcDir.path('app.js'), destDir.path('app.js')),
+        ]);
+};
+
+var bundleSpecs = function () {
+    return generateSpecImportsFile().then(function (specEntryPointPath) {
+        return bundle(specEntryPointPath, destDir.path('spec.js'));
+    });
+};
+
+var bundleTask = function () {
+    if (utils.getEnvName() === 'test') {
+        return bundleSpecs();
+    }
+    return bundleApplication();
+};
+gulp.task('bundle', ['clean'], bundleTask);
+gulp.task('bundle-watch', bundleTask);
+
+
+var sassTask = function () {
+    return gulp.src('app/stylesheets/main.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest(destDir.path('stylesheets')));
+};
+gulp.task('sass', ['clean'], sassTask);
+gulp.task('sass-watch', sassTask);
+
+
+gulp.task('finalize', ['clean'], function () {
+    var manifest = srcDir.read('package.json', 'json');
+
+    // Add "dev" or "test" suffix to name, so Electron will write all data
+    // like cookies and localStorage in separate places for each environment.
+    switch (utils.getEnvName()) {
+        case 'development':
+            manifest.name += '-dev';
+            manifest.productName += ' Dev';
+            break;
+        case 'test':
+            manifest.name += '-test';
+            manifest.productName += ' Test';
+            break;
+    }
+
+    // Copy environment variables to package.json file for easy use
+    // in the running application. This is not official way of doing
+    // things, but also isn't prohibited ;)
+    manifest.env = projectDir.read('config/env_' + utils.getEnvName() + '.json', 'json');
+
+    destDir.write('package.json', manifest);
+});
+
+
+gulp.task('watch', function () {
+    watch('app/**/*.js', batch(function (events, done) {
+        gulp.start('bundle-watch', done);
+    }));
+    watch(paths.copyFromAppDir, { cwd: 'app' }, batch(function (events, done) {
+        gulp.start('copy-watch', done);
+    }));
+    watch('app/**/*.scss', batch(function (events, done) {
+        gulp.start('sass-watch', done);
+    }));
+});
+
+
+gulp.task('build', ['bundle', 'sass', 'copy', 'finalize']);
